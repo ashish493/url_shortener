4a81962413b6f168a7d176e682ceffd9be2fa814
ashish493 <b218008@iiit-bh.ac.in>
2020-11-08 16:38:32 +0530

    Initial Commit

---
+++ shortener/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from hashlib import md5
+
+# Create your models here.
+
+class URL(models.Model):
+    full_url = models.URLField(unique=True)
+    url_hash = models.URLField(unique=True)
+    clicks = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def clicked(self):
+        self.clicks += 1
+        self.save()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.url_hash = md5(self.full_url.encode()).hexdigest()[:10]
+
+        return super().save(*args, **kwargs)
+
+        
